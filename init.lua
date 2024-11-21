64501a48e1a97b19b7a81cf9846280789f58e3a5
Niklas Kahn <niklas.kahn@qbtech.com>
2024-11-21 15:10:03 +0100

    Revert "Change formatter syntax"
    This reverts commit 12fed880f1cd124e9dbebb8a3c07384ba214e63c.

--- init.lua
+++ init.lua
@@ -769,10 +769,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { { 'prettierd', 'prettier' } },
-        typescript = { { 'prettierd', 'prettier' } },
-        javascriptreact = { { 'prettierd', 'prettier' } },
-        typescriptreact = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         sh = { 'shfmt' },
         zsh = { 'beautysh' },
         json = { 'jq' },
