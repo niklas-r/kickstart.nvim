451eaec36d379e1677012bf996ac9959ae947415
Niklas Kahn <niklas.kahn@qbtech.com>
2024-11-21 15:08:58 +0100

    Try vtsls instead of typescript-tools

--- init.lua
+++ init.lua
@@ -672,6 +672,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        vtsls = {},
         --
         bashls = {},
 
