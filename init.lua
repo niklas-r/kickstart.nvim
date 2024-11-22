af7553f8c525488bdce818205d2deeaccdc315d7
Niklas Kahn <niklas.kahn@qbtech.com>
2024-11-22 14:58:28 +0100

    Copy some mini.ai config from LazyVim

--- init.lua
+++ init.lua
@@ -928,6 +928,7 @@ require('lazy').setup({
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
+    event = 'VeryLazy',
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -935,7 +936,35 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require('mini.ai').setup {
+        config = function()
+          -- LazyVim.on_load('which-key.nvim', function()
+          --   vim.schedule(LazyVim.mini.ai_whichkey)
+          -- end)
+          local ai = require 'mini.ai'
+          return {
+            n_lines = 500,
+            custom_textobjects = {
+              o = ai.gen_spec.treesitter { -- code block
+                a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+                i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+              },
+              f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' }, -- function
+              c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' }, -- class
+              t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+              d = { '%f[%d]%d+' }, -- digits
+              e = { -- Word with case
+                { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
+                '^().*()$',
+              },
+              -- i = LazyVim.mini.ai_indent, -- indent
+              -- g = LazyVim.mini.ai_buffer, -- buffer
+              u = ai.gen_spec.function_call(), -- u for "Usage"
+              U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
+            },
+          }
+        end,
+      }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
