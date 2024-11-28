3c60b3bed79250b59613b87a8f82ced5eece6a8c
Niklas Kahn <niklas.kahn@qbtech.com>
2024-11-28 12:27:37 +0100

    Ignore some files types for indent guides

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,35 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      exclude = {
+        buftypes = {
+          'terminal',
+        },
+        filetypes = {
+          '',
+          'norg',
+          'help',
+          'markdown',
+          'dapui_scopes',
+          'dapui_stacks',
+          'dapui_watches',
+          'dapui_breakpoints',
+          'dapui_hover',
+          'dap-repl',
+          'LuaTree',
+          'dbui',
+          'term',
+          'fugitive',
+          'fugitiveblame',
+          'NvimTree',
+          'packer',
+          'neotest-summary',
+          'Outline',
+          'lsp-installer',
+          'mason',
+        },
+      },
+    },
   },
 }
