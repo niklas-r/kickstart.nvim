0fbec778fe256b4dd077aee7a22ed43b5dd4c5cd
Niklas Kahn <niklas.kahn@qbtech.com>
2024-12-05 22:36:48 +0100

    Improve Noice config

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -36,11 +36,26 @@ return {
           },
           view = 'mini',
         },
+        -- This is what long_message_to_split does
+        {
+          filter = { event = 'msg_show', min_height = 20 },
+          view = 'cmdline_output',
+        },
+        -- Extra notify event when messages are not funneled through Noice
+        {
+          filter = { event = 'notify', min_height = 20 },
+          view = 'cmdline_output',
+        },
+      },
+      notify = {
+        enable = true,
+        view = 'notify',
       },
       lsp = {
-        -- progress = {
-        --   enabled = false,
-        -- },
+        progress = {
+          enabled = true,
+          view = 'mini',
+        },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
@@ -52,7 +67,7 @@ return {
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
+        long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = true, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
