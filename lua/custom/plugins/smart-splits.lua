ef95a2347fcdf9e67a31506ba0f9ac473f0d566b
Niklas Kahn <niklas.kahn@qbtech.com>
2024-12-13 09:44:58 +0100

    update smart-splits keybinds

--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -29,15 +29,15 @@ return {
     -- resizing splits
     -- these keymaps will also accept a range,
     -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-    vim.keymap.set('n', '<M-h>', ss.resize_left, { desc = 'Resize split leftward' })
-    vim.keymap.set('n', '<M-j>', ss.resize_down, { desc = 'Resize split downward' })
-    vim.keymap.set('n', '<M-k>', ss.resize_up, { desc = 'Resize split upward' })
-    vim.keymap.set('n', '<M-l>', ss.resize_right, { desc = 'Resize split rightward' })
+    vim.keymap.set('n', '<M-S-h>', ss.resize_left, { desc = 'Resize split leftward' })
+    vim.keymap.set('n', '<M-S-j>', ss.resize_down, { desc = 'Resize split downward' })
+    vim.keymap.set('n', '<M-S-k>', ss.resize_up, { desc = 'Resize split upward' })
+    vim.keymap.set('n', '<M-S-l>', ss.resize_right, { desc = 'Resize split rightward' })
     -- moving between splits
-    vim.keymap.set('n', '<C-h>', ss.move_cursor_left, { desc = 'Move cursor to left split' })
-    vim.keymap.set('n', '<C-j>', ss.move_cursor_down, { desc = 'Move cursor to split below' })
-    vim.keymap.set('n', '<C-k>', ss.move_cursor_up, { desc = 'Move cursor to split above' })
-    vim.keymap.set('n', '<C-l>', ss.move_cursor_right, { desc = 'Move cursor to right split' })
+    vim.keymap.set('n', '<M-h>', ss.move_cursor_left, { desc = 'Move cursor to left split' })
+    vim.keymap.set('n', '<M-j>', ss.move_cursor_down, { desc = 'Move cursor to split below' })
+    vim.keymap.set('n', '<M-k>', ss.move_cursor_up, { desc = 'Move cursor to split above' })
+    vim.keymap.set('n', '<M-l>', ss.move_cursor_right, { desc = 'Move cursor to right split' })
     -- swapping buffers between windows
     vim.keymap.set('n', '<leader>bh', ss.swap_buf_left, { desc = 'Swap buffer with left split' })
     vim.keymap.set('n', '<leader>bj', ss.swap_buf_down, { desc = 'Swap buffer with split below' })
