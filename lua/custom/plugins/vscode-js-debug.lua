1afea3b534cf7c7d101e623b7fa28144d7f8d000
Niklas Kahn <niklas.kahn@qbtech.com>
2024-12-16 15:57:52 +0100

    Fix vscode-js-debug build script

--- lua/custom/plugins/vscode-js-debug.lua
+++ lua/custom/plugins/vscode-js-debug.lua
@@ -1,4 +1,4 @@
 return {
   'microsoft/vscode-js-debug',
-  build = 'npm install --frozen-lockfile --legacy-peer-deps && git reset --hard HEAD && npx gulp vsDebugServerBundle && mv -f dist out',
+  build = 'npm install --frozen-lockfile --legacy-peer-deps && git reset --hard HEAD && npx gulp vsDebugServerBundle && rm -rf ./out && mv -f dist out',
 }
