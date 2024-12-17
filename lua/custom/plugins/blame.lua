03edc134c686e7d45bd1432d7090580b86cadc7a
Niklas Kahn <niklas.kahn@qbtech.com>
2024-12-17 20:48:49 +0100

    Add keymaps to blame

--- lua/custom/plugins/blame.lua
+++ lua/custom/plugins/blame.lua
@@ -2,6 +2,25 @@ return {
   {
     'FabijanZulj/blame.nvim',
     event = 'BufRead',
-    opts = {},
+    config = function(_, opts)
+      require('blame').setup {}
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'BlameViewOpened',
+        callback = function(event)
+          local wk = require 'which-key'
+          local map = function(key, desc)
+            wk.add { key, desc = desc, buffer = event.buf, nowait = true, silent = true, noremap = true }
+          end
+          -- event.buf
+          map('i', 'Blame: Commit info')
+          map('<TAB>', 'Blame: Stack push')
+          map('<BS>', 'Blame: Stack pop')
+          map('<CR>', 'Blame: Show commit')
+          map('<ESC>', 'Blame: close')
+          map('q', 'Blame: close')
+        end,
+      })
+    end,
   },
 }
