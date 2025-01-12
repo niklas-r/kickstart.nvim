ac6837f5fbb21ce8d813a830fbf980db451ee98b
Niklas Kahn <niklas.kahn@qbtech.com>
2025-01-12 22:04:21 +0100

    move all remaps/keymaps to the same file

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -1,3 +1,27 @@
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', function()
+  vim.cmd 'nohlsearch'
+  -- Dismiss "notifications" from packages
+  if package.loaded['notify'] ~= nil then
+    require('notify').dismiss { pending = true, silent = true }
+  end
+  if require 'snacks' ~= nil then
+    require('snacks').notifier.hide()
+  end
+end)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Move selected lines in visual mode
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
