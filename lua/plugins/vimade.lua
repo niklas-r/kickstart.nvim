589facbd741b85507f1e8328023cdfaa782d1d20
Niklas Kahn <niklas.kahn@qbtech.com>
2025-01-17 09:13:28 +0100

    improved dimming on inactive panes

--- lua/plugins/vimade.lua
+++ lua/plugins/vimade.lua
@@ -2,4 +2,18 @@
 return {
   'tadaa/vimade',
   event = 'VeryLazy',
+  opts = {
+    -- Removes line numbers etc
+    recipe = { 'minimalist' },
+
+    -- basebg is used to make dimmed windows look better with transparent themes
+    -- checkout the guide in the documentation for how to pick a good color
+    basebg = { 57, 56, 77 },
+
+    -- Nicer style for inactive pane
+    tint = {
+      fg = { rgb = { 200, 200, 200 }, intensity = 0.5 },
+    },
+    fadelevel = 0.6,
+  },
 }
