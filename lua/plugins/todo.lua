a6403dd1d162f48fc9fbfac6c4a3791d9190397c
Niklas Kahn <niklas.kahn@qbtech.com>
2025-02-12 22:08:39 +0100

    remove alt for TEST todo comments
    they can easily give false positives. such as `case SomeState.FAILED:`

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,7 +2,12 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  opts = {
+    signs = false,
+    keywords = {
+      TEST = { icon = '⏲ ', color = 'test', alt = false },
+    },
+  },
   -- Add snacks pickers
   keys = {
     {
